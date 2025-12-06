d0d882cfd80cfc4eefbba1dec521699048dd01be
banguale <alexander.bangu@mail.utoronto.ca>
2025-12-06 12:46:39 -0500

    added LeftRight deadzone check to tankmode

--- potatomelt/tank_control.cpp
+++ potatomelt/tank_control.cpp
@@ -10,7 +10,10 @@ void handle_tank_mode()
 	// One loop of tank drive!
 
 	int forback = rc_get_forback_trans() * TANK_FORBACK_POWER_SCALE;
-	int leftright = rc_get_leftright() * TANK_TURNING_POWER_SCALE;
+	int leftright = 0;
+	if(!rc_get_is_lr_in_config_deadzone()) {
+		leftright = rc_get_leftright() * TANK_TURNING_POWER_SCALE;
+	}
 	int dir = rc_get_spin_dir();
 
 	motors_on(dir * (forback + leftright), -1 * dir * (forback - leftright));
